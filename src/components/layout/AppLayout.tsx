e3964e98f1e93e10027f45f8eacf610d5ba93621
rocha <rocha.felipe98@gmail.com>
2026-07-26 11:01:54 -0300

    feat: add AppLayout composing sidebar, topbar and drawer

---
+++ src/components/layout/AppLayout.tsx
@@ -0,0 +1,22 @@
+import { useState } from 'react'
+import { Outlet } from 'react-router-dom'
+import { Sidebar } from './Sidebar'
+import { Topbar } from './Topbar'
+import { MobileDrawer } from './MobileDrawer'
+
+export function AppLayout() {
+  const [drawerOpen, setDrawerOpen] = useState(false)
+
+  return (
+    <div className="min-h-screen bg-green-50">
+      <Sidebar />
+      <MobileDrawer open={drawerOpen} onClose={() => setDrawerOpen(false)} />
+      <div className="lg:pl-60">
+        <Topbar onOpenDrawer={() => setDrawerOpen(true)} />
+        <main>
+          <Outlet />
+        </main>
+      </div>
+    </div>
+  )
+}
