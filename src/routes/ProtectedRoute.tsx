ed5d1e665f64004e0d86bd3b413252b99a8cb3ef
rocha <rocha.felipe98@gmail.com>
2026-08-15 00:21:17 -0300

    feat(theme): dark mode for ProtectedRoute

--- src/routes/ProtectedRoute.tsx
+++ src/routes/ProtectedRoute.tsx
@@ -5,7 +5,7 @@ import { useVehicle } from '../context/VehicleContext'
 function Spinner() {
   return (
     <div className="flex h-screen items-center justify-center">
-      <div className="h-10 w-10 animate-spin rounded-full border-4 border-gray-300 border-t-blue-600" />
+      <div className="h-10 w-10 animate-spin rounded-full border-4 border-gray-300 dark:border-gray-600 border-t-blue-600" />
     </div>
   )
 }
